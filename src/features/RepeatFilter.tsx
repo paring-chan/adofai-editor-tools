6cabd8ab71d45532f5b0fd30ee9a7514d361762a
pikokr <parangee9706@gmail.com>
2021-09-07 15:51:44 +0900

    input

--- src/features/RepeatFilter.tsx
+++ src/features/RepeatFilter.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import ExpansionPanel from '../components/ExpansionPanel'
+import { TextField } from '@material-ui/core'
 
 const RepeatFilter = () => {
-    return <ExpansionPanel title="필터 반복(김밥)">와 샌즈</ExpansionPanel>
+    return (
+        <ExpansionPanel title="필터 반복(김밥)">
+            <TextField
+                type="number"
+                inputProps={{
+                    min: 0,
+                }}
+                label="시작타일"
+            />
+        </ExpansionPanel>
+    )
 }
 
 export default RepeatFilter
